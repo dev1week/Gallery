3c94ac68f4c1d5c6f028a52f82a663b4790d8dfe
hanju <the_1_week@naver.com>
2023-08-25 17:07:57 +0900

    #1 -1 Screen 정의하기
    FavoriteImageListScreen.js :
    좋아요 누른 이미지 목록 화면
    ImageDetailScreen.js :
    이미지 상세 화면
    ImageListScreen.js
    전체 이미지 목록 화면

---
+++ src/screen/FavoriteImageListScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import { Typography } from "../components/Typography";
+
+export const FavoriteImageListScreen = () => {
+  return (
+    <View style={{ flex: 1, alignContent: "center", justifyContent: "center" }}>
+      <Typography>Favorite Image List Screen</Typography>
+    </View>
+  );
+};
---
+++ src/screen/ImageDetailScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import { Typography } from "../components/Typography";
+
+export const ImageDetailScreen = (props) => {
+  return (
+    <View style={{ flex: 1, alignContent: "center", justifyContent: "center" }}>
+      <Typography>Image Detail</Typography>
+    </View>
+  );
+};
