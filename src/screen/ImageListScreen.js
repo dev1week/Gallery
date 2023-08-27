a67ec47fc13a5a5b1d06f9afd73fa645189e725c
dev1week <rondo2860@gmail.com>
2023-08-27 23:28:01 +0900

    #8 -1 ImageList Screen에 헤더 적용

--- src/screen/ImageListScreen.js
+++ src/screen/ImageListScreen.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { View } from "react-native";
 import { Typography } from "../components/Typography";
+import { Header } from "../components/Header/Header";
 
 export const ImageListScreen = (props) => {
   return (
-    <View style={{ flext: 1, alignItems: "center", justifyContent: "center" }}>
-      <Typography fontSize={20}>ImageList</Typography>
+    <View style={{ flex: 1 }}>
+      <Header>
+        <Header.Group>
+          <Header.Title title="Image List"></Header.Title>
+        </Header.Group>
+      </Header>
+
+      <View
+        style={{ flext: 1, alignItems: "center", justifyContent: "center" }}
+      ></View>
     </View>
   );
 };
