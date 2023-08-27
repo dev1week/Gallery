9efa72ab1474412cfea625cecf664a3c1608db85
dev1week <rondo2860@gmail.com>
2023-08-27 23:40:47 +0900

    #8 -3 이미지 단일 item 생성
    추후 클릭시 상세 페이지 이동을 위해 버튼 컴포넌트 내에 이미지를 띄우도록 구성함

---
+++ src/components/PhotoListItem.js
@@ -0,0 +1,15 @@
+import React, { useCallback } from "react";
+import { useWindowDimensions } from "react-native";
+import { Button } from "./Button";
+import { RemoteImage } from "./RemoteImage";
+export const PhotoListItem = (props) => {
+  const { width } = useWindowDimensions();
+
+  const onPressItem = useCallback(() => {}, []);
+
+  return (
+    <Button onPress={onPressItem} paddingHorizontal={20} paddingVertical={10}>
+      <RemoteImage url={props.url} width={width - 40} height={width * 1.2} />
+    </Button>
+  );
+};
