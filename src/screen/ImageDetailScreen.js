c6156d5354e35347c3d79097a10f3f04d2561d59
dev1week <rondo2860@gmail.com>
2023-08-28 00:05:41 +0900

    #9 -1 이미지 상세 페이지 헤더 개발

--- src/screen/ImageDetailScreen.js
+++ src/screen/ImageDetailScreen.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { View } from "react-native";
+import { Header } from "../components/Header/Header";
 import { Typography } from "../components/Typography";
 
 export const ImageDetailScreen = (props) => {
   return (
-    <View style={{ flex: 1, alignContent: "center", justifyContent: "center" }}>
-      <Typography>Image Detail</Typography>
+    <View style={{ flext: 1 }}>
+      <Header>
+        <Header.Group>
+          <Header.Icon iconName={"arrow-back"}></Header.Icon>
+          <Header.Title title="IMAGE DETAIL"></Header.Title>
+        </Header.Group>
+      </Header>
+      <View
+        style={{ flex: 1, alignContent: "center", justifyContent: "center" }}
+      >
+        <Typography>Image Detail</Typography>
+      </View>
     </View>
   );
 };
