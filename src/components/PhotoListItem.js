9b42353c7fb549d8cf404b5d8ebd6ff3ed027a69
dev1week <rondo2860@gmail.com>
2023-08-27 23:58:09 +0900

    #8 -6 이미지 클릭시 상세페이지 이동 로직

--- src/components/PhotoListItem.js
+++ src/components/PhotoListItem.js
@@ -3,10 +3,16 @@ import { useWindowDimensions } from "react-native";
 import { Button } from "./Button";
 import { RemoteImage } from "./RemoteImage";
 import { Spacer } from "./Spacer";
+import { useNavigation } from "@react-navigation/native";
 export const PhotoListItem = (props) => {
   const { width } = useWindowDimensions();
+  //이미지 클릭시 상세 화면 전환되도록 네비게이션 추가
+  const navigation = useNavigation();
 
-  const onPressItem = useCallback(() => {}, []);
+  //root stack navigation에 정의한 이미지 상세 페이지 확인하고 오기
+  const onPressItem = useCallback(() => {
+    navigation.navigate("ImageDetail", { url: props.url });
+  }, []);
 
   return (
     <Button onPress={onPressItem} paddingHorizontal={20} paddingVertical={10}>
