75090bfa404143d6daba095f7e0fb94ce32e6c62
dev1week <rondo2860@gmail.com>
2023-08-28 23:04:27 +0900

    #9 -2 뒤로가기 기능 추가

--- src/screen/ImageDetailScreen.js
+++ src/screen/ImageDetailScreen.js
@@ -2,13 +2,22 @@ import React, { useCallback } from "react";
 import { View } from "react-native";
 import { Header } from "../components/Header/Header";
 import { Typography } from "../components/Typography";
+import { useNavigation } from "@react-navigation/native";
 
 export const ImageDetailScreen = (props) => {
+  const navigation = useNavigation();
+
+  const onPressBack = useCallback(() => {
+    navigation.goBack();
+  });
   return (
     <View style={{ flext: 1 }}>
       <Header>
         <Header.Group>
-          <Header.Icon iconName={"arrow-back"}></Header.Icon>
+          <Header.Icon
+            iconName={"arrow-back"}
+            onPress={onPressBack}
+          ></Header.Icon>
           <Header.Title title="IMAGE DETAIL"></Header.Title>
         </Header.Group>
       </Header>
