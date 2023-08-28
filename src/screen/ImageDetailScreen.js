f8e7f5b7bda00c6652a6b224a55fb52e9ee3189c
dev1week <rondo2860@gmail.com>
2023-08-28 23:24:13 +0900

    #9 -3 이미지 상세 화면 구현

--- src/screen/ImageDetailScreen.js
+++ src/screen/ImageDetailScreen.js
@@ -1,18 +1,22 @@
 import React, { useCallback } from "react";
-import { View } from "react-native";
+import { useWindowDimensions, View } from "react-native";
 import { Header } from "../components/Header/Header";
 import { Typography } from "../components/Typography";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { RemoteImage } from "../components/RemoteImage";
 
 export const ImageDetailScreen = (props) => {
   const navigation = useNavigation();
+  const route = useRoute();
+
+  const { width, height } = useWindowDimensions();
 
   const onPressBack = useCallback(() => {
     navigation.goBack();
-  });
+  }, []);
   return (
-    <View style={{ flext: 1 }}>
-      <Header>
+    <View style={{ flex: 1 }}>
+      <Header style={{ flex: 0.5 }}>
         <Header.Group>
           <Header.Icon
             iconName={"arrow-back"}
@@ -24,7 +28,11 @@ export const ImageDetailScreen = (props) => {
       <View
         style={{ flex: 1, alignContent: "center", justifyContent: "center" }}
       >
-        <Typography>Image Detail</Typography>
+        <RemoteImage
+          url={route.params.url}
+          width={width}
+          height={(height * 2) / 3}
+        />
       </View>
     </View>
   );
