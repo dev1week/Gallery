21507f4f67f9a69895357336595bfc00de0c2b72
dev1week <rondo2860@gmail.com>
2023-08-28 23:35:13 +0900

    #9 -4 다운로드 버튼 외형 구현

--- src/screen/ImageDetailScreen.js
+++ src/screen/ImageDetailScreen.js
@@ -4,6 +4,8 @@ import { Header } from "../components/Header/Header";
 import { Typography } from "../components/Typography";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { RemoteImage } from "../components/RemoteImage";
+import { Button } from "../components/Button";
+import { Icon } from "../components/Icon";
 
 export const ImageDetailScreen = (props) => {
   const navigation = useNavigation();
@@ -14,6 +16,9 @@ export const ImageDetailScreen = (props) => {
   const onPressBack = useCallback(() => {
     navigation.goBack();
   }, []);
+
+  const onPressDownload = useCallback(() => {}, []);
+
   return (
     <View style={{ flex: 1 }}>
       <Header style={{ flex: 0.5 }}>
@@ -34,6 +39,21 @@ export const ImageDetailScreen = (props) => {
           height={(height * 2) / 3}
         />
       </View>
+      <Button onPress={onPressDownload}>
+        <View style={{ paddingBottom: 24, backgroundColor: "black" }}>
+          <View
+            style={{
+              height: 52,
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Typography color={"white"}>Download</Typography>
+            <Icon name="download" color="white" size={24}></Icon>
+          </View>
+        </View>
+      </Button>
     </View>
   );
 };
